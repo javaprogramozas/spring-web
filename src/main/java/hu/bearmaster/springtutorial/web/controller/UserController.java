d99f7b8cc4a7abc709c18a897f4ed9a4011ec80e
javaprogramozas <javaprogramozas.hu@gmail.com>
2023-05-23 21:56:14 +0200

    [v31] Sütik

--- src/main/java/hu/bearmaster/springtutorial/web/controller/UserController.java
+++ src/main/java/hu/bearmaster/springtutorial/web/controller/UserController.java
@@ -3,8 +3,11 @@ package hu.bearmaster.springtutorial.web.controller;
 import hu.bearmaster.springtutorial.web.model.User;
 import hu.bearmaster.springtutorial.web.model.request.CreateUserRequest;
 import hu.bearmaster.springtutorial.web.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,17 +24,31 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public String getAllUsers(Model model) {
+    public String getAllUsers(Model model, @CookieValue(required = false) Long visitedUserId) {
         List<User> users = userService.getUsers();
         model.addAttribute("users", users);
+        model.addAttribute("highlighted", visitedUserId);
         return "users";
     }
 
     @GetMapping("/user/{id}")
-    public String getUserById(Model model, @PathVariable long id) {
+    public String getUserById(Model model, @PathVariable long id, HttpServletResponse response) {
         User user = userService.getUserById(id).orElse(null);
         model.addAttribute("user", user);
+
+        if (user != null) {
+            Cookie visitedUserIdCookie = createVisitedUserIdCookie(user.getId());
+            response.addCookie(visitedUserIdCookie);
+        }
+
         return "user";
+    }
+
+    private Cookie createVisitedUserIdCookie(Long userId) {
+        Cookie cookie = new Cookie("visitedUserId", userId.toString());
+        cookie.setPath("/spring-web/");
+        cookie.setMaxAge(60 * 30);
+        return cookie;
     }
 
     @GetMapping("/addUser")
