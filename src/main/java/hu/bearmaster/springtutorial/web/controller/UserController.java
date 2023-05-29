c71218b3f221e5988b2974994222b0b67fdd0ec4
javaprogramozas <javaprogramozas.hu@gmail.com>
2023-05-29 17:21:31 +0200

    [v32] Munkamenet (session)

--- src/main/java/hu/bearmaster/springtutorial/web/controller/UserController.java
+++ src/main/java/hu/bearmaster/springtutorial/web/controller/UserController.java
@@ -3,19 +3,22 @@ package hu.bearmaster.springtutorial.web.controller;
 import hu.bearmaster.springtutorial.web.model.User;
 import hu.bearmaster.springtutorial.web.model.request.CreateUserRequest;
 import hu.bearmaster.springtutorial.web.service.UserService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
 
 @Controller
 public class UserController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService;
 
@@ -24,31 +27,25 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public String getAllUsers(Model model, @CookieValue(required = false) Long visitedUserId) {
+    public String getAllUsers(Model model, @SessionAttribute(required = false) Long visitedUserId,
+                              @SessionAttribute(required = false) User latestUser) {
         List<User> users = userService.getUsers();
         model.addAttribute("users", users);
         model.addAttribute("highlighted", visitedUserId);
+        LOGGER.info("Latest created user: {}", latestUser);
         return "users";
     }
 
     @GetMapping("/user/{id}")
-    public String getUserById(Model model, @PathVariable long id, HttpServletResponse response) {
+    public String getUserById(Model model, @PathVariable long id, HttpSession session) {
         User user = userService.getUserById(id).orElse(null);
         model.addAttribute("user", user);
 
         if (user != null) {
-            Cookie visitedUserIdCookie = createVisitedUserIdCookie(user.getId());
-            response.addCookie(visitedUserIdCookie);
+            session.setAttribute("visitedUserId", user.getId());
         }
 
         return "user";
-    }
-
-    private Cookie createVisitedUserIdCookie(Long userId) {
-        Cookie cookie = new Cookie("visitedUserId", userId.toString());
-        cookie.setPath("/spring-web/");
-        cookie.setMaxAge(60 * 30);
-        return cookie;
     }
 
     @GetMapping("/addUser")
@@ -58,8 +55,9 @@ public class UserController {
     }
 
     @PostMapping("/user")
-    public String addUser(CreateUserRequest request) {
+    public String addUser(CreateUserRequest request, HttpSession session) {
         User createdUser = userService.createUser(request);
+        session.setAttribute("latestUser", createdUser);
         return "redirect:/spring-web/user/" + createdUser.getId();
     }
 }
