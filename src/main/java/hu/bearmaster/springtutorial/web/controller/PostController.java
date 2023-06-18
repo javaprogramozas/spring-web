76122ea2893dd4832046778b9dbdc15e490870bc
javaprogramozas <javaprogramozas.hu@gmail.com>
2023-06-18 18:11:08 +0200

    [v36] @RequestBody annotáció

--- src/main/java/hu/bearmaster/springtutorial/web/controller/PostController.java
+++ src/main/java/hu/bearmaster/springtutorial/web/controller/PostController.java
@@ -1,9 +1,12 @@
 package hu.bearmaster.springtutorial.web.controller;
 
 import hu.bearmaster.springtutorial.web.model.Post;
+import hu.bearmaster.springtutorial.web.model.request.CreatePostRequest;
 import hu.bearmaster.springtutorial.web.service.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +22,10 @@ public class PostController {
     public Post getPostById(@PathVariable long id) {
         Post post = postService.getPostById(id).orElse(null);
         return post;
+    }
+
+    @PostMapping(value = "/post")
+    public Post createNewPost(@RequestBody CreatePostRequest request) {
+        return postService.createNewPost(request);
     }
 }
